511a8e7eea7c1a82c38a4d253b15b9efe4fd434e
Karina Macancela <karymacancela@gmail.com>
2019-10-21 17:30:40 -0400

    Adding JS functionalities

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const bodyEl = document.createElement("body")
+// for modal
+const modal = document.querySelector(".modal")
+const closeButton = document.querySelector(".close-button")
+const modalDiv = document.querySelector(".modal-content")
+const sidenav = document.querySelector(".sidenav")
+
 // "https://pkgstore.datahub.io/core/s-and-p-500-companies/constituents_json/data/64dd3e9582b936b0352fdd826ecd3c95/constituents_json.json"
 
 const url = "https://pkgstore.datahub.io/core/s-and-p-500-companies/constituents_json/data/64dd3e9582b936b0352fdd826ecd3c95/constituents_json.json"
@@ -17,20 +23,20 @@ function getSP500(){
     })
 }
 
+function monthlyData(stock, year, month){
+    
+    return null
+}
+
 function getStocks(symbol){
     // fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=CKKX1N7YZOUYD6IV`)
     fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=CKKX1N7YZOUYD6IV`)
     .then(r => r.json())
     .then(data => {
-      console.log(data["Monthly Time Series"])
+        console.log("Monthly: ", data["Monthly Time Series"])
+        let monthlyData = data["Monthly Time Series"]
 
-        // let arrayOfStocks = []
-        // data.bestMatches.forEach(dataPiece => {
-        //     arrayOfStocks.push(dataPiece)
-        // })
-        // arrayOfStocks.forEach(stock => {
-        //     displayTitle(stock)
-        // })
+        console.log(monthlyData["2019-10-21"])
     })
 }
 
@@ -46,7 +52,7 @@ function displayTitle(stock){
     let buyButton = document.createElement('button')
 
     stockDIV.className = "card"
-    contentDIV.calssName = "content"
+    contentDIV.className = "content"
     headerDIV.className = "header"
     headerDIV.innerText = stock.Name
     metaDIV.className = "meta"
@@ -62,16 +68,66 @@ function displayTitle(stock){
     stockDIV.append(contentDIV, buttonDIV)
     stocksMainDIV.append(stockDIV)
 
+    // adding an event listener for when user clicks on a buy button
     buyButton.addEventListener("click", () => {
-      console.log(stock);
-      getStocks(stock.Symbol)
+        console.log("Stock:", stock);
+        getStocks(stock.Symbol)
+
+        // will trigger modal popup window
+        if (event.target === buyButton) {
+            toggleModal();
+        }  
+        
+        // to add data in modal
+        popUpWindow(stock)
+
+        
     })
 }
 
-// function addStockToDOM(stock){
-//     console.log(stock.contains("name"))
-//     let h1El = document.createElement("h1")
-//         h1El.textContent = stock.name
-//     bodyEl.append(h1El)
-//     console.log(bodyEl)
-// }
+// will search through stocks using symbol
+function searchStocks() {
+    let stocksMainDIV = document.getElementById('stocksMainDIV')
+    // let allStockCards = document.getElementsByClassName('card')
+    let allSpans = stocksMainDIV.querySelectorAll("span")
+    allSpans.forEach(span => {
+        let input = document.getElementById("myInput");
+        // converts input to uppercase, for filtering purposes
+        let filter = input.value.toUpperCase();
+        txtValue = span.textContent
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            span.parentElement.parentElement.style.display = ""
+        } else {
+            span.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
+
+// modal popup box test
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal()
+    }
+}
+
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick)
+
+// when user chooses a stock to add to nav bar or to update
+function popUpWindow(stock){
+    // modalDiv.innerHTML += `<br> ${stock.Symbol}`
+    // console.log(modalDiv)
+    let year = "2019"
+    let jan = document.querySelector("#jan")
+    console.log(modalDiv)
+
+    jan.addEventListener("click", () => {
+        toggleModal()
+        sidenav.textContent = "HIIIIIIII"
+    })
+}
+
