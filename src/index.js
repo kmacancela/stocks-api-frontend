0651b1dc7ab546b525e7d84c010962eb55ba48bf
Karina Macancela <karymacancela@gmail.com>
2019-10-22 09:28:36 -0400

    Adding modal functionalities

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-
-
 let newRound = new LoginPage()
 newRound.render()
 
-function monthlyData(stock, year, month) {
-
-    return null
+function monthlyData(stock, month){
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=ACN&apikey=CKKX1N7YZOUYD6IV`)
+    .then(r => r.json())
+    .then(data => {
+        let monthlyData = data["Monthly Time Series"]
+        let chosenMonthData = monthlyData
+        let keysArray = Object.keys(chosenMonthData)
+        let monthD = keysArray.filter(x => {
+            return x.substring(0,7) == "2019-10"
+        })
+        console.log(monthD)
+        console.log("Chosen Month Data:", monthlyData[monthD])
+    })
 }
 
 
@@ -44,15 +52,37 @@ closeButton.addEventListener("click", toggleModal)
 window.addEventListener("click", windowOnClick)
 
 // when user chooses a stock to add to nav bar or to update
-function popUpWindow(stock) {
+function popUpWindow(stock){
     // modalDiv.innerHTML += `<br> ${stock.Symbol}`
     // console.log(modalDiv)
     let year = "2019"
     let jan = document.querySelector("#jan")
-    console.log(modalDiv)
+    // let jan2 = documen
+    // let monthBtns = modalDiv.querySelectorAll("button")
+    // console.log(monthBtns)
+    // console.log(modalDiv)
 
-    jan.addEventListener("click", () => {
-        toggleModal()
-        sidenav.textContent = "HIIIIIIII"
+    // jan.addEventListener("click", () => {
+    //     toggleModal()
+    //     sidenav.textContent = "HIIIIIIII"
+    // })
+
+    jan.addEventListener("click", evt => {
+        console.log(evt.target.id)
+        // if (evt.target.id = "jan"){
+        //     console.log("Hi")
+        // }
+    })
+
+    let numEl = document.createElement("input")
+        numEl.value = "Enter num of stocks"
+    
+    modalDiv.append(numEl)
+
+    let nextBtn = modalDiv.querySelector("#nextBtn")
+    // console.log("Next: ", nextBtn)
+    nextBtn.addEventListener("click", evt => {
+        console.log("Next:", nextBtn)
+        monthlyData("ACN", "January")
     })
 }
